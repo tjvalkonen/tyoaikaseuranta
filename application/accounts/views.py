3c814a0bac09ea05d4e9910ca29c42f9c5ad2421
Tomas Valkonen <tomas.valkonen@helsinki.fi>
2020-04-30 20:54:29 +0300

    Tietoja tunnuksen tietosivulle

--- application/accounts/views.py
+++ application/accounts/views.py
@@ -10,16 +10,6 @@ from application.auth.forms import ModifyAccountForm
 def accounts_index():
     return render_template("accounts/list.html", accounts = User.query.all())
 
-# @app.route("/projects/<project_id>/", methods=["POST"])
-# @login_required
-# def projects_set_done(project_id):
-
-#    t = Project.query.get(project_id)
-#    t.done = True
-#    db.session().commit()
-  
-#    return redirect(url_for("projects_index"))
-
 @app.route("/accounts/<account_id>/delete/", methods=["POST"])
 @login_required(role="ADMIN")
 def accounts_delete(account_id):
@@ -34,7 +24,7 @@ def accounts_delete(account_id):
 @login_required()
 def accounts_modify_form(account_id):
   
-    return render_template("accounts/modifyAccount.html", account_id = account_id, form = ModifyAccountForm())
+    return render_template("accounts/modifyAccount.html", account_id = account_id, form = ModifyAccountForm(), projects = User.workdone_in_projects())
 
 @app.route("/accounts/<account_id>/modify", methods=["POST"])
 @login_required()
--- application/auth/models.py
+++ application/auth/models.py
@@ -1,5 +1,6 @@
 from application import db
 from application.models import Base
+from flask_login import current_user
 
 from sqlalchemy.sql import text
 
@@ -33,3 +34,17 @@ class User(Base):
 
     def roles(self):
         return [self.role]
+
+    @staticmethod
+    def workdone_in_projects():
+        stmt = text("SELECT Project.id, Project.name, Project.done, SUM(Task.time) FROM Project"
+                     " LEFT JOIN Task ON Task.project_id = Project.id"
+                     " WHERE (Task.taskstatus IS 'Actual') AND (Task.account_id = :current_user_id)"
+                     " GROUP BY Project.id").params(current_user_id=current_user.id)
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1], "done":row[2], "time":row[3]})
+
+        return response
